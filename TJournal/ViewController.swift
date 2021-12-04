c426e9eb6d000975e661a5296063722250315005
ast <shibzuhov2000@mail.ru>
2021-12-04 04:44:43 +0300

    Initial Commit

---
+++ TJournal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TJournal
+//
+//  Created by Astemir Shibzuhov on 12/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
